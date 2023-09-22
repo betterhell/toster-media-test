aaafccd95d2104763a157a669ccf1637f01fd65a
betterhell <dev.betterhell@gmail.com>
2023-09-22 12:05:56 +0300

    final commit (i think :))

--- scripts.js
+++ scripts.js
@@ -1,16 +1,48 @@
-let mainImage = document.querySelector(".product__main-image")
+// main slider
 const productImages = document.querySelectorAll(".product__images__image")
+const productLine = document.querySelector(".product__main-images__container")
 
-console.log(mainImage.style.transition)
-for (let image of productImages) {
+productImages.forEach(image => {
     image.addEventListener("click", () => {
         let currentImage = document.querySelector(".product__images__image.current")
+        let offset = image.dataset.offset
 
         if (currentImage) {
             currentImage.classList.remove("current")
         }
         image.classList.add("current")
-        mainImage.style.transition = "all 2s ease-in"
-        mainImage.setAttribute("src", image.src)
+        productLine.style.transform = `translateX(${offset}px)`
     })
-}
+})
+
+
+// star rating
+let stars = document.querySelectorAll(".star__item"),
+    starsActive,
+    starsSelect;
+
+stars.forEach((element, index) => {
+
+    element.addEventListener('mouseover', () => {
+        starsActive = Array.prototype.slice.call(stars, 0, index + 1);
+        starsActive.forEach((star) => {
+            star.classList.add("star__item_active");
+        });
+    });
+
+    element.addEventListener('mouseout', () => {
+        stars.forEach((star) => {
+            star.classList.remove("star__item_active");
+        });
+    });
+
+    element.addEventListener('click', () => {
+        stars.forEach((star) => {
+            star.classList.remove("star__item_select");
+        });
+        starsActive.forEach((star) => {
+            star.classList.add("star__item_select");
+        });
+        starsSelect = starsActive;
+    });
+});
